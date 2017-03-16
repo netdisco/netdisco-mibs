07ea23e27a2a2fa808fc9f4ae6ce577544390f25
Oliver Gorwits <oliver@cpan.org>
2017-03-16 19:01:10 +0000

    factor out mib_index2 parsing

--- extras/scripts/lib/Helpers.pm
+++ extras/scripts/lib/Helpers.pm
@@ -7,10 +7,12 @@ use charnames ':full';
 binmode STDOUT, ':utf8';
 
 use File::Temp;
+use File::Copy;
+use File::Spec::Functions qw(splitdir catfile);
 use Term::ANSIColor qw(:constants);
 
 use Exporter 'import';
-our @EXPORT = qw(status blank);
+our @EXPORT = qw(status blank build_index parse_index2);
 
 sub blank {
   print "\r\e[K"; # blank line
@@ -32,6 +34,67 @@ sub status {
   $i = (!defined $i) ? "\N{BRAILLE PATTERN DOTS-2345678}" : $spinner{$i};
   blank();
   print YELLOW, "$i ", CYAN, $note, RESET;
+}
+
+sub build_index {
+  my ($bundle, $keep) = @_;
+  my (%mib_for, %file_for);
+
+  my $tmpdir = File::Temp->newdir();
+  $ENV{SNMP_PERSISTENT_DIR} = $tmpdir->dirname;
+  qx(snmptranslate -IR sysName 2>&1 >/dev/null);
+  my $newmibdirs = $ENV{MIBDIRS} .":$bundle";
+  qx(snmptranslate -M'$newmibdirs' -IR sysName 2>\&1 >/dev/null);
+  $ENV{SNMP_PERSISTENT_DIR} = "$ENV{MIBHOME}/extras/indexes";
+
+  # read the MIBs that net-snmp likes (file '3' is the vendor)
+  # TODO: make this work also for rfc:net-snmp
+  open(my $cache, '<', "$tmpdir/mib_indexes/3") or die $!;
+  while (my $line = <$cache>) {
+    next if $line =~ m/^DIR /;
+    my ($mib, $file) = ($line =~ m/^(\S+)\s+(\S+)$/);
+
+    # store mib-file mapping
+    $mib_for{$file} = $mib;
+    $file_for{$mib} = $file;
+  }
+  close $cache;
+
+  # keep a copy of the mib index file to help the dev
+  if ($keep) {
+    mkdir(catfile($bundle, 'ignore'));
+    copy("$tmpdir/mib_indexes/3", catfile($bundle, 'ignore', 'dotindex.txt'));
+  }
+
+  return (\%mib_for, \%file_for);
+}
+
+sub parse_index2 {
+  my $indexfile = catfile($ENV{SNMP_PERSISTENT_DIR}, 'mib_index2.txt');
+  if (! -f $indexfile) {
+    print "error: missing mib_index2.txt (run mkindex)\n";
+    exit(1);
+  }
+
+  # load mib_index2.txt mapping all current known MIBs
+  my (%vendor_for, %oldfile_for, %mibs_for, @allmibs, $currvendor);
+  open(my $index, '<', $indexfile) or die $!;
+  while (my $line = <$index>) {
+    next if $line =~ m/^MIB Index/ or $line =~ m/^\s*$/;
+    if ($line =~ m/^DIR\s+(\S+)/) {
+      $currvendor = (grep {m/\S/} splitdir($1))[-1];
+      next;
+    }
+    my ($mib, $file) = ($line =~ m/^(\S+)\s+(\S+)$/);
+
+    $oldfile_for{$mib} = $file;
+    $vendor_for{$mib} = $currvendor;
+    push @{$mibs_for{$currvendor}}, $mib;
+    push @allmibs, $mib;
+  }
+  close $index;
+
+  return (\%vendor_for, \%oldfile_for, \%mibs_for, \@allmibs);
 }
 
 1;
