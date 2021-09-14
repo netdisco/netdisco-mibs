ad833d560c711a80dde4f320d95a0f40aa377d43
Oliver Gorwits <oliver@cpan.org>
2021-09-14 21:57:46 +0100

    new scripts not needing indexes

--- EXTRAS/scripts/lib/Helpers.pm
+++ EXTRAS/scripts/lib/Helpers.pm
@@ -6,13 +6,14 @@ use warnings;
 use charnames ':full';
 binmode STDOUT, ':utf8';
 
-use File::Temp;
-use File::Copy;
-use File::Spec::Functions qw(splitdir catfile);
+use Cwd 'realpath';
+use File::Slurper 'read_text';
+use File::Spec::Functions qw(splitdir catfile catdir);
 use Term::ANSIColor qw(:constants);
+use Try::Tiny;
 
 use Exporter 'import';
-our @EXPORT = qw(status blank build_index parse_index2);
+our @EXPORT = qw(status blank build_index mkindex);
 
 if (!defined $ENV{MIBHOME}) {
   print "error: must define \$MIBHOME (where the MIB dirs live)\n";
@@ -23,11 +24,99 @@ if (!defined $ENV{MIBHOME}) {
 $ENV{LC_COLLATE} = 'C';
 
 $ENV{SNMPCONFPATH} = '';
-$ENV{SNMP_PERSISTENT_DIR} = "$ENV{MIBHOME}/EXTRAS/indexes";
+$ENV{SNMP_PERSISTENT_DIR} = catdir($ENV{MIBHOME}, 'EXTRAS', 'indexes');
 $ENV{MIBS} = 'SNMPv2-MIB';
-$ENV{MIBDIRS} = "$ENV{MIBHOME}/net-snmp:$ENV{MIBHOME}/rfc";
+$ENV{MIBDIRS} = catdir($ENV{MIBHOME}, 'net-snmp') .':'. catdir($ENV{MIBHOME}, 'rfc');
+
+# Given a directory ($target) where some MIBs are waiting, grep them for
+# DEFINITIONS statements to build a map of file->MIB and MIB<->[files]
+# also works when given a plain MIB file.
+# Will bomb when MIB name has illegal chars,
+#                or file has multiple MIB defs
+#                or MIB is defined multiple times in the bundle
+sub build_index {
+  my $target = shift;
+  my (%mib_file);
+
+  my %files = (-f $target ? ($target => $target)
+    : ( map {catfile( (splitdir($_))[-2,-1] ) => $_} grep {-f} glob(catdir(realpath($target), '*')) ));
+
+  while (my ($fileref, $filepath) = each %files) {
+    my $content = try { read_text($filepath, 'latin1') } or next;
+    $content =~ s/ ^ \s* -- .* $ //mxg;
+
+    my @matches = ( $content =~ m{  \s* ([A-Za-z][\w-]*+) \s+ DEFINITIONS \s* ::= \s* BEGIN }mxg );
+    next unless scalar @matches;
+
+    if (scalar @matches > 1) {
+      blank();
+      print RED, "\N{HEAVY BALLOT X} stopped: ", MAGENTA, $filepath, CYAN,
+        ' contains multiple MIB DEFINITIONS: ', RESET, (join ',', @matches), "\n";
+      exit (1) unless $ENV{ONLY_SQUAWK};
+    }
+
+    my $mib = $matches[0];
+    if (($mib !~ m/^[A-Z][A-Za-z0-9-]*$/) or ($mib =~ m/--/) or ($mib =~ m/-$/)) {
+      blank();
+      print RED, "\N{HEAVY BALLOT X} stopped: ", MAGENTA, $mib, CYAN,
+        ' is named using invalid characters in ', RESET, $filepath, "\n";
+      exit (1) unless $ENV{ONLY_SQUAWK};
+    }
+
+    if (exists $mib_file{$mib}) {
+      blank();
+      print RED, "\N{HEAVY BALLOT X} stopped: ", MAGENTA, $mib, CYAN,
+        ' from ', RESET, $mib_file{$mib}, CYAN,
+        ' is redefined in ', RESET $fileref, "\n";
+    }
+
+    $mib_file{$mib} = $fileref;
+  }
+
+  #use DDP; p %mib_file;
+  return \%mib_file;
+}
+
+# Scan all the MIBs in $ENV{MIBHOME} and return maps of:
+#   file->MIB, MIB->[files], vendor->[MIBs], MIB->[vendors]
+sub mkindex {
+  my ($mib_for_file, $mib_files, $vendor_mibs, $mib_vendors) = ({}, {}, {});
+
+  # TODO put rfc and net-snmp in different order?
+  foreach my $vendor (map {(splitdir($_))[-1]} grep {-d} glob(catdir($ENV{MIBHOME},'*'))) {
+    next if $vendor =~ m/^(?:EXTRAS)$/ or $vendor =~ m/\./;
+
+    status($vendor);
+    my $file_for = build_index(catdir($ENV{MIBHOME}, $vendor));
+
+    # file->MIB
+    $mib_for_file = { %$mib_for_file, reverse %$file_for };
+
+    # MIB->[files]
+    map { push @{ $mib_files->{ $_ } }, $file_for->{$_} } keys %$file_for;
+
+    # vendor->[MIBs]
+    $vendor_mibs->{$vendor} = [ sort {$a cmp $b} keys %$file_for ];
+
+    # MIB->[vendors]
+    map { push @{ $mib_vendors->{ $_ } }, $vendor } keys %$file_for;
+  }
+  blank();
+
+  # clean up the lookup values
+  foreach my $mib (keys %$mib_files) {
+    $mib_files->{$mib} = [sort {$a cmp $b} @{ $mib_files->{$mib} }];
+  }
+
+  printf "\N{HEAVY CHECK MARK} Index rebuilt (%s vendors, %s mibs).\n",
+    (scalar keys %$vendor_mibs), (scalar keys %$mib_for_file);
+
+  #use DDP; map {p $_} ($mib_for_file, $vendor_mibs, $mib_vendors);
+  return ($mib_for_file, $mib_files, $vendor_mibs, $mib_vendors);
+}
 
 sub blank {
+  select((select(STDOUT), $|=1)[0]);
   print "\r\e[K"; # blank line
 }
 
@@ -46,76 +135,8 @@ sub status {
   );
   $i = (!defined $i) ? "\N{BRAILLE PATTERN DOTS-2345678}" : $spinner{$i};
   blank();
+  select((select(STDOUT), $|=1)[0]);
   print YELLOW, "$i ", CYAN, $note, RESET;
-}
-
-# Given a directory ($bundle) where some new/untested MIBs are waiting, run
-# snmptranslate over those MIBs and return maps of MIB<->file and file<->MIB.
-# TODO: make this work also for rfc:net-snmp MIBs.
-sub build_index {
-  my ($bundle, $keep) = @_;
-  my (%mib_for, %file_for);
-
-  # change net-snmp persistent dir and establish index baseline
-  my $tmpdir = File::Temp->newdir();
-  $ENV{SNMP_PERSISTENT_DIR} = $tmpdir->dirname;
-  qx(snmptranslate -IR sysName 2>&1 >/dev/null);
-  # now run snmptranslate to get the new index file
-  my $newmibdirs = $ENV{MIBDIRS} .":$bundle";
-  qx(snmptranslate -M'$newmibdirs' -IR sysName 2>&1 >/dev/null);
-  # restore persistent dir
-  $ENV{SNMP_PERSISTENT_DIR} = "$ENV{MIBHOME}/EXTRAS/indexes";
-
-  # read the index file ('2' is the new/untested MIBs over baseline)
-  open(my $cache, '<', "$tmpdir/mib_indexes/2") or die $!;
-  while (my $line = <$cache>) {
-    next if $line =~ m/^DIR /;
-    my ($mib, $file) = ($line =~ m/^(\S+)\s+(.+)/);
-
-    # store mib-file mapping
-    $mib_for{$file} = $mib;
-    $file_for{$mib} = $file;
-  }
-  close $cache;
-
-  # keep a copy of the mib index file to help the dev
-  if ($keep) {
-    mkdir(catfile($bundle, 'skip'));
-    copy("$tmpdir/mib_indexes/2", catfile($bundle, 'skip', 'dotindex.txt'));
-  }
-
-  return (\%mib_for, \%file_for);
-}
-
-# Read the netdisco-mibs mib_index2.txt file and return all the data within.
-# maps of: MIB<->file, MIB<->vendor, vendor<->[MIBs]
-#   also a list of all MIBs known to netdisco-mibs
-sub parse_index2 {
-  my $indexfile = catfile($ENV{SNMP_PERSISTENT_DIR}, 'mib_index2.txt');
-  if (! -f $indexfile) {
-    print "error: missing mib_index2.txt (run mkindex)\n";
-    exit(1);
-  }
-
-  # load mib_index2.txt mapping all current known MIBs
-  my (%vendor_for, %oldfile_for, %mibs_for, @allmibs, $currvendor);
-  open(my $index, '<', $indexfile) or die $!;
-  while (my $line = <$index>) {
-    next if $line =~ m/^MIB Index/ or $line =~ m/^\s*$/;
-    if ($line =~ m/^VENDOR\s+(.+)/) {
-      $currvendor = (grep {m/\S/} splitdir($1))[-1];
-      next;
-    }
-    my ($mib, $file) = ($line =~ m/^(\S+)\s+(.+)/);
-
-    $oldfile_for{$mib} = $file;
-    $vendor_for{$mib} = $currvendor;
-    push @{$mibs_for{$currvendor}}, $mib;
-    push @allmibs, $mib;
-  }
-  close $index;
-
-  return (\%vendor_for, \%oldfile_for, \%mibs_for, \@allmibs);
 }
 
 1;
